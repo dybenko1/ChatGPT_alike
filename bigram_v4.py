6d365d504e433308e1e34de49cc0e469e92bb1e8
DybG <5200749+dybenko1@users.noreply.github.com>
2026-03-13 18:25:28 +0800

    Added dropout as regularization technique since we increased the size (depth) of the network. It did not run on my laptop (took years just for the first 500 iters out of 5,000). I ran it in the Lab GPU and still took like 15 minutes, but the results were mind-blowing!!
    step 4500: train loss 1.0284, val loss 1.5177
    
    And the generated text is:
    Ress'd himself; his ruing like behappared;
    He deserves of a bawdy done, a virtuous in her;
    And with a fear rest his old faithful sique
    Until weed to reason as high a look,
    Is not foundangerous proger duty is all. Yet bare
    What I see to censure you? the father yours
    Make your pleant bassel
    The will beggar that your royal grace, which
    There hudges have, wah'd but my inclination,
    You have King Julian's sake, and unnacy's enter.
    
    KING RICHARD III:
    Yea, ay, wedded to me, gentle purpose
    To our succeed

--- bigram_v4.py
+++ bigram_v4.py
@@ -4,22 +4,21 @@ from torch.nn import functional as F
 
 
 # hyperparameters
-batch_size = 64
-block_size = 256 
+batch_size = 32
+block_size = 8 
 max_iters = 5_000
 eval_interval = 500
-learning_rate = 3e-4 # self-attention block canoot tolerate high lr
-device = 'mps' if torch.mps.is_available() else 'cpu' # cpu because for this small model the mps. option is slower
+learning_rate = 1e-3 # self-attention block canot tolerate high lr
+device = 'cpu' if torch.mps.is_available() else 'cpu' # cpu because for this small model the mps. option is slower
 eval_iters = 200
-n_embd = 384 # Dimension of embedding vectors
-n_head = 6
-n_layer = 6
-dropout = 0.2
+n_embd = 32 # Dimension of embedding vectors
+
+#dropout = 0.2
 
 #-------------
 
-
 torch.manual_seed(1337)
+
 #!wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
 with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
@@ -79,7 +78,8 @@ class Head(nn.Module):
         # to assign it to the module with this "register_buffer" function.
         # So that line would be equivalent to "self.tril = torch.tril(torch.ones...)))""
 
-        self.dropout = nn.Dropout(dropout)  # Adding dropout as regularization.
+
+        #self.dropout = nn.Dropout(dropout) # What is this?
 
     def forward(self, x):
         # input of size (batch, time-step, channels)
@@ -92,9 +92,8 @@ class Head(nn.Module):
         ##TODO Corregir linea del wei.masked**************************************************************
         wei = wei.masked_fill(self.tril[:T,:T]==0, float('-inf')) # (B, T, T)
         wei = F.softmax(wei, dim=-1) # (B, T, T)
-        wei = self.dropout(wei) ## Randomly preventing some of the nodes from communicating 
 
-        v = self.value(x) # (B, T, hs). Aunque segun Andrej the dimension should be (B, T, C)
+        v = self.value(x) # (B, T, hs).
 
         out = wei @ v #  (B, T, T) @ (B, T, hs) ---> (B, T, hs)
         return out
@@ -105,48 +104,9 @@ class MultiHeadAttention(nn.Module):
     def __init__(self, num_heads, head_size):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
-        self.proj = nn.Linear(n_embd, n_embd) # The pirpose of this projector is to take the output of the attention heads and project them back into the residual pathway. 
-        ## It is to map the output (of the heads) back to the data stream (x) dimension, so. there are not dimensionality mismatches; specially useful in situations when 
-        ## n_emd != n_heads * head_size
-        ## It also allows the model to learn wot to "mix and combine the information" from the different heads together, rather than just treating them as parallel streams of data
-        ## In our example there is no dimension msimatch, but the projector is still usefull bceause if we were to change the n_heads or the heads size it would always help to match dimensions
-        self.dropout = nn.Dropout(dropout)  # Adding dropout as regularization.
 
     def forward(self, x):
-      out = torch.cat([h(x) for h in self.heads], dim=-1)
-      out = self.proj(out)
-      return out
-
-class FeedForward(nn.Module):
-  def __init__(self, n_embd):
-    super().__init__()
-    self.net = nn.Sequential(
-      nn.Linear(n_embd, 4 * n_embd),
-      nn.ReLU(),
-      nn.Linear(4 * n_embd, n_embd), # For the "Residual". This is the projection layer back to the residual pathway. Here we fix a dimension mismatch
-      nn.Dropout(dropout), # Adding dropout as regularization. This is added right before the residual conenction  back to the residual pathway
-    )
-
-  def forward(self, x): # This linear layer is on token level, i.e. indepently each token "thinks" about the info they retrieve in the past (self-attention layer)
-    return self.net(x)
-
-class Block(nn.Module):
-  def __init__(self, n_embd, n_head):
-    super().__init__()
-    head_size = n_embd // n_head
-    self.sa = MultiHeadAttention(n_head, head_size)
-    self.ffwd = FeedForward(n_embd)
-    self.ln1 = nn.LayerNorm(n_embd) ## Andrej showed the equivalent function of this function that come from a paper. And in here we deviate from the original "Attention is all you need" paper and apply it before, not after the attention heads
-    ## Ojo con las dimensiones. Aca la operacion de nromalziacion (mean of 1 and sd of 1) happens over 32 numbers (n_embd), so the batch and time as a batch dimen, both of them
-    ## So this is like a "per token" transformation. It normalizes the features at inizialization, but since therformula has the gamma and beta parameters, later the
-    ## norm Layer norm would produce outputs that may not be "standard" (meand 1 and sd 1)
-    ## OJO: I explain the importance of this layer in the commit.
-    self.ln2 = nn.LayerNorm(n_embd)
-
-  def forward(self, x):
-    x = x + self.sa(self.ln1(x)) # This is the residual part. We add the "x + ". Here we then added the Layer Norm 1
-    x = x + self.ffwd(self.ln2(x)) # This is the residual part. We add the "x + ". Here we then added the Layer Norm 2
-    return x
+        return torch.cat([h(x) for h in self.heads], dim=-1)
 
 
 
@@ -160,12 +120,12 @@ class BigramLanguageModel(nn.Module):
         # to go from the token embeddings to the logits we need a linear layer
         # We do not just need to encode the tokens given their identity (the word/meaning itself), but also the position
         self.position_embedding_table = nn.Embedding(block_size, n_embd) # each position from 0 to block_size -1 will get its own embedding vector
-        # Adding blocks (transformers)
-        self.blocks = nn.Sequential(*[Block(n_embd, n_head) for _ in range(n_layer)]) # Changing the format of the block 
-        self.ln_f = nn.LayerNorm(n_embd) # Putting a final Layer Norm
-        #self.sa_heads = MultiHeadAttention(4, n_embd//4) # i.e. 4 heads of 8-dimensional self-attention. # Interesting because with a single head the dim was 32,
-
-        #self.ffwd = FeedForward(n_embd)
+        
+        self.sa_heads = MultiHeadAttention(4, n_embd//4) # i.e. 4 heads of 8-dimensional self-attention. # Interesting because with a single head the dim was 32,
+        ## the same dim of the embedding. But now since we have 4 heads and we are going to concatenate the outputs we set the head_size to 8 = 32/4
+        ## so what i think is that we are giving each head 8 'different' (i think) dimensions to play with and 'give' meaning to those directions.
+        ## so the entire 32 dim representation of a token is more rich (complex, i.e. has (can hold) more meaning).
+        ## Andrej says that this is like a group convolution, because istead of having a big convolution we do one of groups.
         self.lm_head = nn.Linear(n_embd, vocab_size) # lm_head : language model head. !! Why this dimension?
         ## OjO! we already abandoned the representation of tokens as integers. we now represent them as a vector of 32 dimensions
         ## that's the reason of n_embd=32. With this many directions (dims) each token can hold complex rich 
@@ -188,15 +148,8 @@ class BigramLanguageModel(nn.Module):
         x = tok_emb + pos_emb # (B,T,C). pos_em no tiene dim B (batch), pero pytorch hace su magia al sumar eso a cada 
         ## batch. at the end the embedding of position i is the same regardless the batch. We just add that to our vector of n_embd dimensions
 
-        #x = self.sa_heads(x) # apply the multi-head attention
-        # Forward through transformer blocks 
-        x = self.blocks(x)
-        # When we developed the multi-head attention head we right away calculated the logits. This did not allow
-        # the tokens to "think" what they find about the other tokens (with the multi-head attention), this is why we implement
-        # a linear layer for the tokens to "grasp" what they found when they communicate with each other (in the self-attention layer)
-        #x = self.ffwd(x) # (B,T, C)
-        logits = self.lm_head(x) # (B, T, vocab_size). 
-        
+        x = self.sa_heads(x) # apply the multi-head attention
+        logits = self.lm_head(x) # (B, T, vocab_size)
 
         if targets is None:
             loss = None
